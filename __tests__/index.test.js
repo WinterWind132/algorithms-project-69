9a4b1614d73394460d7764e77dde592d7e18f29b
Winter Wind <casino13538@gmail.com>
2024-08-19 11:59:49 +0300

    Added features

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,13 +1,13 @@
 import search from '../src/index';
 
 test('search function', () => {
-  const documents = [
-    { name: 'doc1', content: 'This is a test document' },
-    { name: 'doc2', content: 'Another document for testing' },
-    { name: 'doc3', content: 'No matches here' },
-  ];
+  const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a document and a pint!" };
+  const doc2 = { id: 'doc2', text: 'Another document for testing' };
+  const doc3 = { id: 'doc3', text: 'No matches here' };
+  const documents = [doc1, doc2, doc3];
 
-  expect(search(documents, 'test')).toEqual(['doc1', 'doc2']);
+  expect(search(documents, 'pint')).toEqual(['doc1']);
+  expect(search(documents, 'pint!')).toEqual(['doc1']);
   expect(search(documents, 'document')).toEqual(['doc1', 'doc2']);
   expect(search(documents, 'matches')).toEqual(['doc3']);
   expect(search(documents, 'none')).toEqual([]);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
+function processText(text) {
+  const cleanedText = text.replace(/[^\w\s]/g, ' ').toLowerCase();
+  return cleanedText;
+}
+
 export default function search(documents, query) {
-    return documents
-      .filter(doc => doc.content.includes(query))
-      .map(doc => doc.name);
-  }
+  const processedQuery = processText(query).trim();
+  console.log('Processed Query:', processedQuery);
+  const queryRegex = new RegExp(`\\b${processedQuery}\\b`, 'i');
+  console.log('Query Regex:', queryRegex);
+  return documents
+    .filter(doc => {
+      const processedText = processText(doc.text);
+      console.log('Processed Text:', processedText);
+      console.log('Test Result:', queryRegex.test(processedText));
+      return queryRegex.test(processedText);
+    })
+    .map(doc => doc.id);
+}
